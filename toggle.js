335511336e9e4b8680f50f01f735eef0c58a0951
crocJohnDee <johnn.barendrecht@hotmail.com>
2019-08-19 12:21:15 +0200

    first cmmit

---
+++ toggle.js
@@ -0,0 +1,17 @@
+let button = document.querySelector("button");
+let list = document.querySelector("ul");
+
+let listVisible = false;
+
+button.addEventListener("click", function listener() {
+    listVisible = !listVisible;
+    if (listVisible) {
+        list.style.display = "block";
+        button.innerHTML = "Get them out of here"
+    } else {
+        list.style.display = "none";
+        button.innerHTML = " Whow favorite destinatioons";
+    }
+})
+
+
